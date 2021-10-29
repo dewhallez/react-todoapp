1f5a272460e84ee49e2bca44d2ac34f3f0b84944
akinwale <4412196+dewhallez@users.noreply.github.com>
2021-10-29 13:35:35 -0400

    Fix function error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
 import React from 'react';
+import './App.css';
 import ListItems from './ListItems';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {faTrash} from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faTrash);
 
@@ -21,14 +21,7 @@ class App extends React.Component {
     this.deleteItem = this.deleteItem.bind(this);
     this.setUpdate = this.setUpdate.bind(this);
   }
-  handleInput(e){
-    this.setState({
-      currentItem:{
-        text: e.target.value,
-        ket:Date.now()
-      }
-    })
-  }
+  
   addItem(e){
     e.preventDefault();
     const newItem = this.state.currentItem;
@@ -44,37 +37,58 @@ class App extends React.Component {
       })
     }
   }
-  deleteItem(key) {
-    const filteredItems = this.state.items.filter(item => item.key !== key);
-    this.setState( {items: filteredItems })
-    }
-
-  setUpdate(text, key) {
-    const items = this.state.items;
-    items.map(item=>{
-      if(item.key===key){
-        item.text = text;
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        ket:Date.now()
       }
-    })
-    this.setState({items: items
     })
   }
 
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter(item => 
+      item.key !== key);
+    this.setState( {
+      items: filteredItems 
+    })
+  }
 
+  setUpdate(text,key) {
+    console.log("items:" + this.state.items);
+    const items = this.state.items;
+    items.map(item=>{
+      if(item.key===key){
+        console.log(item.key + " " + key)
+        item.text = text;
+      }
+    })
+    this.setState({ 
+      items: items
+    })
+   }
 
   render(){
     return (
       <div className="App">
         <header>
-        <form id="to-do-form" onSubmit={this,this.addItem}>
-          <input type="text" placeholder="Enter text"
-          value={this.state.currentItem.text} onChange={this.handleInput} />
+
+        <form id="to-do-form" onSubmit={this.addItem}>
+
+          <input type="text" placeholder="Enter task"
+          value={this.state.currentItem.text} onChange={this.handleInput}></input>
+
           <button type="submit">Add</button>
+
         </form>
+
+        <p> {this.state.items.text} </p>
+
+          <ListItems items = {this.state.items} 
+            deleteItem = {this.deleteItem}
+            setUpdate = {this.setUpdate} />
       </header>
-      <ListItems items = {this.state.items} 
-        deleteItem = {this.deleteItem}
-        setUpdate = {this.setUpdate} ></ListItems>
+      
       </div>
     );
   }
