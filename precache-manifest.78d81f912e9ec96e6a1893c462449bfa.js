6c081923fe579f2aacd62ea558feb87c1ab6c2e3
akinwale <4412196+dewhallez@users.noreply.github.com>
2021-10-29 12:46:25 -0400

    Updates

---
+++ precache-manifest.78d81f912e9ec96e6a1893c462449bfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac30e53b3d975083609d1ba14be8442",
+    "url": "/react-todoapp.git/index.html"
+  },
+  {
+    "revision": "1c023043a1917219b97c",
+    "url": "/react-todoapp.git/static/css/main.76b160d6.chunk.css"
+  },
+  {
+    "revision": "35fdf4d59e9591628d85",
+    "url": "/react-todoapp.git/static/js/2.374dd83c.chunk.js"
+  },
+  {
+    "revision": "337690bdb71522eaada8e2b2e5b3e79a",
+    "url": "/react-todoapp.git/static/js/2.374dd83c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788c6300a17590e38fec",
+    "url": "/react-todoapp.git/static/js/3.a258db08.chunk.js"
+  },
+  {
+    "revision": "1c023043a1917219b97c",
+    "url": "/react-todoapp.git/static/js/main.42f42c3f.chunk.js"
+  },
+  {
+    "revision": "49574cdec1332c71290d",
+    "url": "/react-todoapp.git/static/js/runtime-main.72f7c8e2.js"
+  }
+]);
